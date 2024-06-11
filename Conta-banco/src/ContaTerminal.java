28306f5f54fe1dbc1e0c5076b9718cc49fdf1716
Igor Mendes <153562801+igormendes1997@users.noreply.github.com>
2024-06-11 15:43:04 -0300

    Update ContaTerminal.java

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     
+        //conhecer e importa a classe Scanner
+
+        //Exibir as mensagens para o nosso usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
     }
 }
